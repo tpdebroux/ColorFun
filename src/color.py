d8dc5cb1791aa0480f3e4759ca4e4c71c95b506f
tpdebroux <tpdebroux@wisc.edu>
2018-11-23 13:37:52 -0600

    fixed bug with calculating next hue

--- src/color.py
+++ src/color.py
@@ -1,3 +1,16 @@
+def Hue_2_RGB( v1, v2, vH ):
+   if (vH < 0):
+        vH += 1
+   if (vH > 1):
+        vH -= 1
+   if ((6 * vH) < 1):
+        return (v1 + (v2 - v1) * 6 * vH)
+   if (( 2 * vH) < 1):
+        return v2
+   if (( 3 * vH) < 2):
+        return (v1 + ( v2 - v1 ) * ((2 / 3) - vH) * 6)
+   return v1
+
 print('Enter a color in RGB form:\n')
 
 r = input('R: ')
@@ -29,6 +42,7 @@ while(not isinstance(b, int)):
         print('B must be decimal value (0-255)')
         b = input('B: ')
 
+print('Input color: ')
 print(r,g,b)
 
 valR = r / 255
@@ -68,6 +82,27 @@ else:
         H += 1
     if (H > 1):
         H -= 1
-print(H,S,L)
+
 H2 = (H + 0.5) % 1
-print(H2)
+
+if (S == 0):
+   R_comp = L * 255
+   G_comp = L * 255
+   B_comp = L * 255
+else:
+   if(L < 0.5):
+       var_2 = L * (1 + S)
+   else:
+       var_2 = (L + S) - (S * L)
+
+   var_1 = 2 * L - var_2
+
+   R_comp = 255 * Hue_2_RGB(var_1, var_2, H2 + (1 / 3))
+   G_comp = 255 * Hue_2_RGB(var_1, var_2, H2)
+   B_comp = 255 * Hue_2_RGB(var_1, var_2, H2 - (1 / 3))
+
+R_comp = int(round(R_comp))
+G_comp = int(round(G_comp))
+B_comp = int(round(B_comp))
+print ('Complimentary color: ')
+print (R_comp, G_comp, B_comp)
