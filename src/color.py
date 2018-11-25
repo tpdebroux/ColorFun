670baf06d86011d986073eaa1640db214f3b1e3a
tpdebroux <tpdebroux@wisc.edu>
2018-11-25 12:01:07 -0600

    reformatting into nicely partitioned functions

--- src/color.py
+++ src/color.py
@@ -1,4 +1,4 @@
-def Hue_2_RGB( v1, v2, vH ):
+def HuetoRGB( v1, v2, vH ):
    if (vH < 0):
         vH += 1
    if (vH > 1):
@@ -11,10 +11,81 @@ def Hue_2_RGB( v1, v2, vH ):
         return (v1 + ( v2 - v1 ) * ((2 / 3) - vH) * 6)
    return v1
 
-print('Enter a color in RGB form:\n')
+def HSLtoRGB( H, S, L):
+    if (S == 0): #it is an easy conversion if there is no saturation
+       R = L * 255
+       G = L * 255
+       B = L * 255
+    else:
+       if(L < 0.5):
+           var_2 = L * (1 + S)
+       else:
+           var_2 = (L + S) - (S * L)
 
+       var_1 = 2 * L - var_2
+
+       R = 255 * HuetoRGB(var_1, var_2, H + (1 / 3))
+       G = 255 * HuetoRGB(var_1, var_2, H)
+       B = 255 * HuetoRGB(var_1, var_2, H - (1 / 3))
+       return [R,G,B]
+
+def RGBtoHSL( r, g, b):
+    valR = r / 255
+    valG = g / 255
+    valB = b / 255
+
+    minVal = min(valR, valG, valB)
+    maxVal = max(valR, valG, valB)
+    diffVal = maxVal - minVal
+
+    L = (maxVal + minVal) / 2
+
+    if (maxVal == 0):#this means all the values are 0 (aka black)
+        H = 0
+        S = 0
+    else:
+        if (L < 0.5):
+            S = diffVal / (maxVal + minVal)
+        else:
+            S = diffVal / (2 - maxVal - minVal)
+
+        if(diffVal == 0):
+            deltaR = deltaG = deltaB = 0
+        else:
+            deltaR = (((maxVal - valR) / 6) + (diffVal / 2)) / diffVal
+            deltaG = (((maxVal - valG) / 6) + (diffVal / 2)) / diffVal
+            deltaB = (((maxVal - valB) / 6) + (diffVal / 2)) / diffVal
+
+        if (valR== maxVal):
+            H = deltaB - deltaG
+        elif (valG == maxVal ):
+            H = (1 / 3) + deltaR - deltaB
+        elif (valB == maxVal):
+            H = (2 / 3) + deltaG - deltaR
+
+        if (H < 0):
+            H += 1
+        if (H > 1):
+            H -= 1
+
+        return [H,S,L]
+def getComplement( r, g, b ):
+    HSL = RGBtoHSL(r, g, b)
+    H = HSL[0]
+    S = HSL[1]
+    L = HSL[2]
+    H = (H + 0.5) % 1
+    RGB = HSLtoRGB(H, S, L)
+    R = int(round(RGB[0]))
+    G = int(round(RGB[1]))
+    B = int(round(RGB[2]))
+    print('Complement color: ')
+    print(R,G,B)
+
+print('Enter a color in RGB form:\n')
+#get rgb values from user, and catch bad input
 r = input('R: ')
-#print (isinstance(r, int))
+
 while((not isinstance(r, int))):
     try:
         r = int(r)
@@ -44,65 +115,5 @@ while(not isinstance(b, int)):
 
 print('Input color: ')
 print(r,g,b)
-
-valR = r / 255
-valG = g / 255
-valB = b / 255
-
-minVal = min(valR, valG, valB)
-maxVal = max(valR, valG, valB)
-diffVal = maxVal - minVal
-
-L = (maxVal + minVal) / 2
-
-if (maxVal == 0):
-    H = 0
-    S = 0
-else:
-    if (L < 0.5):
-        S = diffVal / (maxVal + minVal)
-    else:
-        S = diffVal / (2 - maxVal - minVal)
-
-    if(diffVal == 0):
-        deltaR = deltaG = deltaB = 0
-    else:
-        deltaR = (((maxVal - valR) / 6) + (diffVal / 2)) / diffVal
-        deltaG = (((maxVal - valG) / 6) + (diffVal / 2)) / diffVal
-        deltaB = (((maxVal - valB) / 6) + (diffVal / 2)) / diffVal
-
-    if (valR== maxVal):
-        H = deltaB - deltaG
-    elif (valG == maxVal ):
-        H = (1 / 3) + deltaR - deltaB
-    elif (valB == maxVal):
-        H = (2 / 3) + deltaG - deltaR
-
-    if (H < 0):
-        H += 1
-    if (H > 1):
-        H -= 1
-
-H2 = (H + 0.5) % 1
-
-if (S == 0):
-   R_comp = L * 255
-   G_comp = L * 255
-   B_comp = L * 255
-else:
-   if(L < 0.5):
-       var_2 = L * (1 + S)
-   else:
-       var_2 = (L + S) - (S * L)
-
-   var_1 = 2 * L - var_2
-
-   R_comp = 255 * Hue_2_RGB(var_1, var_2, H2 + (1 / 3))
-   G_comp = 255 * Hue_2_RGB(var_1, var_2, H2)
-   B_comp = 255 * Hue_2_RGB(var_1, var_2, H2 - (1 / 3))
-
-R_comp = int(round(R_comp))
-G_comp = int(round(G_comp))
-B_comp = int(round(B_comp))
-print ('Complementary color: ')
-print (R_comp, G_comp, B_comp)
+getComplement(r,g,b)
+getAnalogous(r,g,b)
